b2b9d061396d25418a1fa0f1e9120a8afff29d4d
kzhdev <kzhdev@users.noreply.github.com>
2026-02-03 18:59:58 +0000

    deploy: 1fd541ba4d99bf8309e4430f3c3ae751f3301713

--- api/search/all_d.js
+++ api/search/all_d.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['data_20structures_0',['data structures',['../index.html#autotoc_md12',1,'Cache-Optimized Data Structures'],['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md36',1,'Data Structures']]],
-  ['debug_20assertions_20in_20production_1',['❌ Pitfall 5: Debug Assertions in Production',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md105',1,'']]],
-  ['default_2',['Compiled Library (Default)',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md67',1,'']]],
+  ['data_20structures_0',['data structures',['../index.html#autotoc_md12',1,'Cache-Optimized Data Structures'],['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md41',1,'Data Structures']]],
+  ['debug_20assertions_20in_20production_1',['❌ Pitfall 5: Debug Assertions in Production',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md66',1,'']]],
+  ['default_2',['Compiled Library (Default)',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md115',1,'']]],
   ['deletelevel_3',['deleteLevel',['../classslick_1_1orderbook_1_1OrderBookL2.html#a92faf069bffc184aca513b6211957563',1,'slick::orderbook::OrderBookL2']]],
   ['deleteorder_4',['deleteOrder',['../classslick_1_1orderbook_1_1OrderBookL3.html#a36e6971be4c1b8a84c9e8574180fdd43',1,'slick::orderbook::OrderBookL3']]],
-  ['deletion_5',['3. Quantity=0 Deletion',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md52',1,'']]],
-  ['design_6',['design',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md62',1,'2. Policy-Based Design'],['../index.html#autotoc_md14',1,'Lock-Free Design']]],
-  ['design_20patterns_7',['Design Patterns',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md60',1,'']]],
+  ['deletion_5',['3. Quantity=0 Deletion',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md98',1,'']]],
+  ['design_6',['design',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md110',1,'2. Policy-Based Design'],['../index.html#autotoc_md14',1,'Lock-Free Design']]],
+  ['design_20patterns_7',['Design Patterns',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md108',1,'']]],
   ['documentation_8',['📚 Documentation',['../index.html#autotoc_md10',1,'']]]
 ];
