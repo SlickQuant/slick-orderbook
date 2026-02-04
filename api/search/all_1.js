c8448d3bbd9fad2d8d87e29d7a90acc76150c234
kzhdev <kzhdev@users.noreply.github.com>
2026-02-04 03:31:35 +0000

    deploy: 6e758267ce80450ee04c502b254a568a64267274

--- api/search/all_1.js
+++ api/search/all_1.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['1_20cached_20topofbook_0',['1. Cached TopOfBook',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md77',1,'']]],
-  ['1_20choose_20the_20right_20orderbook_20type_1',['1. Choose the Right Orderbook Type',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md39',1,'']]],
-  ['1_20template_20based_20polymorphism_20crtp_2',['1. Template-Based Polymorphism (CRTP)',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md104',1,'']]],
-  ['10_20use_20release_20builds_3',['10. Use Release Builds',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md58',1,'']]],
-  ['1_3a_20compiler_20optimizations_4',['Technique 1: Compiler Optimizations',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md74',1,'']]],
-  ['1_3a_20copying_20orderbooks_5',['❌ Pitfall 1: Copying Orderbooks',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md63',1,'']]]
+  ['1_20cached_20topofbook_0',['1. Cached TopOfBook',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md49',1,'']]],
+  ['1_20choose_20the_20right_20orderbook_20type_1',['1. Choose the Right Orderbook Type',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md88',1,'']]],
+  ['1_20template_20based_20polymorphism_20crtp_2',['1. Template-Based Polymorphism (CRTP)',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2ARCHITECTURE.html#autotoc_md60',1,'']]],
+  ['10_20use_20release_20builds_3',['10. Use Release Builds',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md97',1,'']]],
+  ['1_3a_20compiler_20optimizations_4',['Technique 1: Compiler Optimizations',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md107',1,'']]],
+  ['1_3a_20copying_20orderbooks_5',['❌ Pitfall 1: Copying Orderbooks',['../md__2home_2runner_2work_2slick-orderbook_2slick-orderbook_2docs_2PERFORMANCE.html#autotoc_md100',1,'']]]
 ];
